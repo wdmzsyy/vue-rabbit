4b5fa89fb29065121f94dff859d72fbe838f156b
wdmzsyy <2037525426@qq.com>
2026-04-25 20:23:20 +0800

    Home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,14 @@ export function getBannerAPI() {
         url: '/home/banner'
     })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @returns {*}
+ */
+export const findNewAPI = () => { //导出函数
+    return httpInstance({
+        url: '/home/new'   //httpInstance({ url: ... }) 返回的是一个 Promise 对象（因为 axios 请求是异步的）
+    })
+}
