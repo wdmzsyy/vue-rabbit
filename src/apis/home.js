144d629e5c71625f62f7d2ec4264b7abdbf3157a
wdmzsyy <2037525426@qq.com>
2026-04-26 10:55:46 +0800

    Home-product列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -17,8 +17,25 @@ export const findNewAPI = () => { //导出函数
     })
 } 
 
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
 export const getHotAPI = () => {
     return httpInstance({
         url: '/home/hot'
+    })
+}
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+    return httpInstance({
+        url: '/home/goods'
     })
 }
