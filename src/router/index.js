26aed28148b6ee8a4d1b2b2efe1dbfba67fd1dea
wdmzsyy <2037525426@qq.com>
2026-05-02 15:58:26 +0800

    二级分类-路由滚动行为体验优化

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // path和component对应关系的位置
   routes: [
     {
       path: '/',
@@ -36,6 +37,12 @@ const router = createRouter({
       component: Login
     }
   ],
+  // 路由滚动行为定制
+  scrollBehavior() {
+    return {
+      top: 0 
+    }
+  }
 })
 
 export default router
