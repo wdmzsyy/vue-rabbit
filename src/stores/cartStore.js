bc55ba08dc3abce21930ba2abdaa131872752ac5
wdmzsyy <2037525426@qq.com>
2026-05-05 16:04:03 +0800

    购物车-列表-统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -49,6 +49,11 @@ export const useCartStore = defineStore('cart', () => {
     // 2.总价 所有项的count*price之和
     const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price, 0))
 
+    // 3.已选择数量
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a + c.count, 0))
+    // 4.已选择商品价钱合计
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a + c.count * c.price, 0))
+    
     // 是否全选
     //every：如果遍历item.selected都为true，则返回true赋值给isAll
     const isAll = computed(() => cartList.value.every((item) => item.selected))
@@ -57,6 +62,8 @@ export const useCartStore = defineStore('cart', () => {
         cartList,
         allCount,
         allPrice,
+        selectedCount,
+        selectedPrice,
         isAll,
         addCart,
         delCart,
