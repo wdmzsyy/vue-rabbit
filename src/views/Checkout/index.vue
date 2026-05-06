3a38894045e3a42c76a64e7a97fd4181de6b9e3d
wdmzsyy <2037525426@qq.com>
2026-05-06 21:21:51 +0800

    结算页-提交订单业务实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,12 @@
 <script setup>
-import { getCheckInfoAPI } from '@/apis/checkout'
+import { getCheckInfoAPI, createOrderAPI } from '@/apis/checkout'
+import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
+import { useCartStore } from '@/stores/cartStore'
+const cartStore = useCartStore()
+const router = useRouter()
+
+// 获取结算信息
 const checkInfo = ref({}) // 订单对象
 const curAddress = ref({}) // 地址对象
 const getCheckInfo = async () => {
@@ -28,6 +34,32 @@ const confirm = () => {
     showDialog.value = false
     activeAddress.value = {}
     // Error：什么都不选直接确定，curAddress就变为空了，显示也为空
+}
+
+// 创建订单
+const createOrder = async () => {
+    const res = await createOrderAPI({
+        deliveryTimeType: 1,
+        payType: 1,
+        payChannel: 1,
+        buyerMessage: '',
+        goods: checkInfo.value.goods.map(item => {      // map映射，返回数组
+            return {
+                skuId: item.skuId,
+                count: item.count
+            }
+        }),
+        addressId: curAddress.value.id
+    })
+    const orderId = res.result.id
+    router.push({
+        path: '/pay',
+        query: {
+            id: orderId
+        }
+    })
+    // 更新购物车
+    cartStore.updateNewList()
 }
 </script>
 
