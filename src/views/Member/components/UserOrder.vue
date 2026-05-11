4f9180469583c795a067c3f9a63963e0b27f2e4f
wdmzsyy <2037525426@qq.com>
2026-05-11 17:57:26 +0800

    会员中心-我的订单-tab切换实现

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -26,11 +26,18 @@ const getOrderList = async () => {
 }
 onMounted(() => getOrderList())
 
+// tab切换（将type交给orderState重新发送网络请求）
+const tabChange = (type) => {
+    console.log(type);
+    params.value.orderState = type
+    getOrderList()
+}
+
 </script>
 
 <template>
     <div class="order-container">
-        <el-tabs>
+        <el-tabs @tab-change="tabChange">
             <!-- tab切换 -->
             <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
