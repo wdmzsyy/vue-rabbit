ef2662e4709c15f080f0609168b8ac8d6c64542f
wdmzsyy <2037525426@qq.com>
2026-05-11 16:24:40 +0800

    会员中心-功能梳理和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,6 +58,20 @@ const router = createRouter({
           path: 'paycallback',
           component: PayBack
         },
+        {
+          path: 'member',
+          component: Member,
+          children: [     // 三级路由，path路径和index.vue的<RouterLink to="/member/user">个人中心</RouterLink>对应
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
+        }
       ]
     },
     {
